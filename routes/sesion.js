7e74104688df147672db03b3c37f53e4eec17515
pedrocore73 <pjimenez@corenetworks.es>
2019-10-30 19:06:41 +0100

    sesiones

--- routes/sesion.js
+++ routes/sesion.js
@@ -3,6 +3,24 @@ var app = express();
 
 var Sesion = require('../models/sesion');
 
+app.get('/:id', (req, res)=>{
+
+    var id = req.params.id;
+
+    Sesion.find({idUsuario: id}).exec((error, data)=>{
+        if(error) {
+            return res.status(400).json({
+                error: error
+            })
+        }
+
+        res.status(200).json({
+            sesiones: data
+        })
+    })
+
+})
+
 app.post('/', (req, res)=>{
     var body = req.body;
     if (body.login) {
